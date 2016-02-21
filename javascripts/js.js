26998ca684f30e9190dcfb8825b18e00d81b453f
Tina Han <sangheetinahan@gmail.com>
2016-02-21 12:51:13 -0500

    hehe

---
+++ javascripts/js.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+  $('#img1').mouseenter(function() {
+    $('#Nat').addClass('underline');
+  }).mouseleave(function() {
+    $('#Nat').removeClass('underline');
+  });
+
+  $('#img2').mouseenter(function() {
+    $('#SOW').addClass('underline');
+  }).mouseleave(function() {
+    $('#SOW').removeClass('underline');
+  });
+
+
+  function deselect(e) {
+    $('.pop').slideFadeToggle(function() {
+      e.removeClass('selected');
+    });
+  }
+
+  $(function() {
+    $('#name').on('click', function() {
+      if($(this).hasClass('selected')) {
+        deselect($(this));
+      } else {
+        $(this).addClass('selected');
+        $('.pop').slideFadeToggle();
+      }
+      return false;
+    });
+
+    $('.close').on('click', function() {
+      deselect($('#name'));
+      return false;
+    });
+  });
+
+  $.fn.slideFadeToggle = function(easing, callback) {
+    return this.animate({ opacity: 'toggle', height: 'toggle' }, 'fast', easing, callback);
+  };
+});
